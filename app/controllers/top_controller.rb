1b1f94d056bb69f962a76ea5e59b1dc7a3453a19
Yoshihiro Imaida <yimaida@example.com>
2017-09-21 19:46:31 +0900

    mod: TopControllerの不要コードを削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,35 +1,8 @@
-require "google/cloud/vision"
-
 class TopController < ApplicationController
   def index
     if user_signed_in?
       flash.keep
       redirect_to '/topics'
     end
-  #   client = Aws::Rekognition::Client.new(
-  #     region: 'us-west-2',
-  #   )
-  #   @moderation_resp = client.detect_moderation_labels(
-  #     image: {
-  #       bytes: File.read('./noro.jpg')
-  #     }
-  #   )
-  #   @label_resp = client.detect_labels(
-  #     image: {
-  #       bytes: File.read('./noro.jpg')
-  #     },
-  #     max_labels: 10,
-  #   )
-  #
-  #   project_id = "gungnir-001"
-  #   image_path = "./noro.jpg"
-  #   # image_path = "./snis.jpg"
-  #
-  #   vision = Google::Cloud::Vision.new(
-  #     project: project_id,
-  #     keyfile: "config/gungnir-001-viewer.json"
-  #   )
-  #   @image = vision.image image_path
-  #   @safe_search = @image.safe_search
   end
 end
