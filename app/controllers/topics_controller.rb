62ff7850b94f5b8d991f969ef9bc070f059ed248
Yoshihiro Imaida <yimaida@example.com>
2017-09-23 15:59:00 +0900

    mod: トピック一覧の表示順を更新日付順に変更

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,7 @@ class TopicsController < ApplicationController
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   def index
-    @topics = Topic.all
+    @topics = Topic.all.order(updated_at: :desc)
   end
 
   def select_image
